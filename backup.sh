e6dfb7a3afe9aeb92e2239412e8491bbae6c972a
AmazonPython <38388847+Ryboster@users.noreply.github.com>
2025-07-18 02:45:00 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -4,10 +4,6 @@ PASSWORD=$(cat /mnt/secrets/password)
 AUTH_TOKEN=$(cat /mnt/secrets/token)
 USERNAME=$(cat /mnt/secrets/username)
 
-echo $PASSWORD
-echo $AUTH_TOKEN
-echo $USERNAME
-
 cp /home/pi/website/*.sqlite3 /home/pi/website_backup/
 cp /home/pi/website/*.crt /home/pi/website_backup/
 cp /home/pi/website/*.key /home/pi/website_backup/
@@ -18,4 +14,3 @@ cd /home/pi/website_backup
 git add .
 git commit -m "Backup changes: $(date)"
 git push https://$USERNAME:$AUTH_TOKEN@github.com/SnekByte-Studios/website_backup.git
-echo "Backup and push completed successfully!"
