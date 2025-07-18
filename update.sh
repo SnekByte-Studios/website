5192bc7176f172fd225bb3ae935f23baf1f26d9f
snek@pi4 <kakashise@o2.pl>
2025-07-18 02:44:19 +0100

    renamed batch files

--- update.sh
+++ update.sh
@@ -4,5 +4,6 @@ AUTH_TOKEN=$(cat /mnt/secrets/token)
 cd /home/pi/website
 
 git pull https://$USERNAME:$AUTH_TOKEN@github.com/SnekByte-Studios/website.git
+source website_env/bin/activate
 python3 manage.py migrate --noinput
 python3 manage.py collectstatic --noinput
