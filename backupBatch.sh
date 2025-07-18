698180563f60765066696a19403198c067929c47
snek@pi4 <kakashise@o2.pl>
2025-07-18 02:20:35 +0100

    Backup changes: Fri Jul 18 02:20:35 BST 2025

---
+++ backupBatch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+PASSWORD=$(cat /mnt/password)
+AUTH_TOKEN=$(cat /mnt/token)
+USERNAME=$(cat /mnt/username)
+
+cp /home/pi/website/*.sqlite3 /home/pi/website_backup/
+cp /home/pi/website/*.crt /home/pi/website_backup/
+cp /home/pi/website/*.key /home/pi/website_backup/
+cp /home/pi/website/*.pem /home/pi/website_backup/
+cp -r /home/pi/website/logs /home/pi/website_backup/
+cd /home/pi/website
+
+git add .
+git commit -m "Backup changes: $(date)"
+git push https://$USERNAME:$AUTH_TOKEN@github.com/$USERNAME/website_backup.git
+echo "Backup and push completed successfully!"
