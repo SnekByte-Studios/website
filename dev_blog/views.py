1caba0e90e505a08a7ec2abdec7e564bb7b4ff77
23017998 <23017998@uhi.ac.uk>
2025-07-18 19:09:59 +0100

    improved logger. Added cookie verification on every endpoint

--- dev_blog/views.py
+++ dev_blog/views.py
@@ -9,18 +9,20 @@ from datetime import datetime
 
 db = CRUD()
 
+
+### GET
+### TEMPLATE ENDPOINTS
+###
 def home(request):
+    if isSession(request) and not db.isSessionValid(request): logout(request)
     pageFile = os.path.join("pages", "home.html")
     return render(request, pageFile)
 
 
 def changelogSite(request):
+    if isSession(request) and not db.isSessionValid(request): logout(request)
     pageFile = os.path.join("pages", "changelog.html")
-    allChangelogs = db.read("changelogs", "logs")  # List of tuples
-    print("CHANGELOGS: ")
-    print(allChangelogs)
-
-    # Convert to list of dicts and parse the date
+    allChangelogs = db.read("changelogs", "logs")
     logs = []
     for item in allChangelogs:
         logs.append({
@@ -31,42 +33,77 @@ def changelogSite(request):
             "changes": ast.literal_eval(item[2]),
         })
 
-    # Sort logs by parsed date descending
     logs.sort(key=lambda x: x["parsed_date"], reverse=True)
-
-    # Remove parsed_date before sending to template if you don't need it there
     for log in logs:
         del log["parsed_date"]
-
-    # Send sorted logs to template
     context = {"logs": logs}
     return render(request, pageFile, context)
 
+
+### POST
+### LOGIN / LOGOUT ENDPOINTS
+###
 def login(request):
     if request.method == "POST":
+        print("login request received")
         submitted_data = request.POST
         enteredUsername, enteredPassword = submitted_data["username"], submitted_data["password"]
         enteredHash = db.generateHash(enteredUsername, enteredPassword)
+
         if db.areCredsValid(enteredHash):
             sessionToken = db.generateSessionToken()
-            db.create("sessions", "sessions", (str(enteredHash), str(sessionToken), int(time.time())))                 
+            db.create("sessions", "sessions", (str(enteredHash), str(sessionToken), int(time.time())))    
+            print(f"logged in user: {enteredUsername}")             
             return appendUserDataToResponse(redirect("/home"), enteredUsername, enteredHash, sessionToken)
     return redirect("/home")
 
 def logout(request):
     userCookies = request.COOKIES
-    print("logging out ...")
     db.delete("sessions", "sessions", "id", userCookies.get("ID", ""))
+    print("logged out user: " + userCookies.get("USERNAME"))
     return wipeUserCookies(redirect("/home"))
 
+### POST
+### FUNCTIONAL ENDPOINTS
+###
 def addNewChangelogEntry(request):
-    if request.method == "POST":
+    if isSession(request) and request.method == "POST":
         if db.isSessionValid(request):
             submitted_data = request.POST
             correctedDate = ".".join(submitted_data['date'].replace("-", ".").split(".")[::-1])
             db.create("changelogs", "logs", (correctedDate, str(submitted_data['title']), str(submitted_data.getlist('changes'))), ("Date", "Title", "Changes"))
+            print(f"user {request.COOKIES.get('USERNAME')} CREATED new changelog entry; title: {str(submitted_data['title'])}\ndate: {correctedDate}")
             return redirect("/changelog")
-        else: return wipeUserCookies(redirect("/home"))      
+        else: return wipeUserCookies(redirect("/home"))
+    return redirect("/home")
+
+def removeChangelogEntry(request):
+    if isSession(request) and request.method == "POST":
+        if db.isSessionValid(request):
+            db.delete("changelogs", "logs", "ID", request.POST["changeID"])
+            print(f"user {request.COOKIES.get('USERNAME')} DELETED changelog entry; ID: {request.POST["changeID"]}")
+        else: logout(request)
+    return redirect("/changelog")
+
+def editChangelogEntry(request):
+    if isSession(request) and request.method == "POST":
+        if db.isSessionValid(request):
+            submitted_data = request.POST
+            correctedDate = ".".join(submitted_data['date'].replace("-", ".").split(".")[::-1])
+            values = [correctedDate, submitted_data['title'], str(submitted_data.getlist('changes'))]
+            db.update(database="changelogs", table="logs", columns=submitted_data.getlist("columns"), whereColumn="ID", whereValue=submitted_data["dbid"], values=values)
+            print(f"user {request.COOKIES.get('USERNAME')} EDITED changelog entry; new title: {submitted_data['title']}\nnew date: {correctedDate}")
+        else: logout(request)
+    return redirect("/changelog")
+
+
+###
+### HELPER FUNCTIONS
+###
+def isSession(request):
+    if request.COOKIES.get("ID") and request.COOKIES.get("SESH_TOKEN") and request.COOKIES.get("USERNAME"):
+        return True
+    else: return False
 
 def appendUserDataToResponse(response, username, checksum, sessionToken):
       response.set_cookie("USERNAME", str(username))
@@ -79,19 +116,3 @@ def wipeUserCookies(response):
     response.delete_cookie("ID")
     response.delete_cookie("SESH_TOKEN")
     return response
-
-def removeChangelogEntry(request):
-    if request.method == "POST":
-        if db.isSessionValid(request):
-            db.delete("changelogs", "logs", "ID", request.POST["changeID"])
-        else: logout(request)
-    return redirect("/changelog")
-
-def editChangelogEntry(request):
-    if request.method == "POST" and db.isSessionValid(request):
-        submitted_data = request.POST
-        correctedDate = ".".join(submitted_data['date'].replace("-", ".").split(".")[::-1])
-        values = [correctedDate, submitted_data['title'], str(submitted_data.getlist('changes'))]
-        db.update(database="changelogs", table="logs", columns=submitted_data.getlist("columns"), whereColumn="ID", whereValue=submitted_data["dbid"], values=values)
-    else: logout(request)
-    return redirect("/changelog")
